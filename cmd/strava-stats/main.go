48ee763e3e68e726e69c5d5fab75f567d5e8a098
Junie AI <junie@jetbrains.com>
2025-11-25 12:18:43 -0800

    Fix: Unignore cmd/strava-stats directory and add main.go

---
+++ cmd/strava-stats/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	port := ":8080"
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("web/templates/index.html")
+		if err != nil {
+			http.Error(w, "Could not load template", http.StatusInternalServerError)
+			log.Printf("Error parsing template: %v", err)
+			return
+		}
+		if err := tmpl.Execute(w, nil); err != nil {
+			log.Printf("Error executing template: %v", err)
+		}
+	})
+	
+	server := &http.Server{
+		Addr:         port,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	fmt.Printf("Starting server on http://localhost%s\n", port)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Could not start server: %s\n", err)
+	}
+}
